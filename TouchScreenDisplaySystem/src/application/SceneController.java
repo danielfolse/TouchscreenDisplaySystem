5253b3ef5898de470fa70081ff5e9628a8b4698a
danielfolse <90349765+danielfolse@users.noreply.github.com>
2022-12-03 12:18:10 -0700

    academics branching

--- TouchScreenDisplaySystem/src/application/SceneController.java
+++ TouchScreenDisplaySystem/src/application/SceneController.java
@@ -1,103 +1,80 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.apache.commons.lang.time.StopWatch;
+import java.io.FileWriter;
 
-public class SceneController {
+public class SceneController implements Initializable{
 	private Stage stage; 
 	private Scene scene; 
-	private Parent root; 
+	private Parent root;
+	//StopWatch watch;
+	@FXML
+	GridPane sideBar;
+	@FXML
+	GridPane homeSideBar;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		TranslateTransition translate = new TranslateTransition();
+		translateGrid(-763, translate, sideBar);
+		translateGrid(763, translate, homeSideBar);
+	}
 	
 	public void goToHomePage(ActionEvent e) throws IOException{
+		//watch.start();
 		String pathOfNewFile = "/pages/homepage.fxml";
-		root = FXMLLoader.load(getClass().getResource(pathOfNewFile)); 
-		scene = new Scene(root); 
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow(); 
-		stage.setScene(scene);
-		stage.setMaximized(true);
-		stage.setResizable(true);
-		stage.show();
+		addLink(e, pathOfNewFile);
 	}
 	
 	public void goToIdlePage(ActionEvent e) throws IOException{
 		String pathOfNewFile = "/pages/idle-page.fxml";
-		root = FXMLLoader.load(getClass().getResource(pathOfNewFile)); 
-		scene = new Scene(root); 
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow(); 
-		stage.setScene(scene);
-		stage.setMaximized(true);
-		stage.setResizable(true);
-		stage.show();
+		addLink(e, pathOfNewFile);
+		//watch.stop();
+		//WriteStatsToFile(watch);
 	}
 	
 	public void goToUndergradPage(ActionEvent e) throws IOException{
 		String pathOfNewFile = "/pages/undergraduate-program.fxml";
-		root = FXMLLoader.load(getClass().getResource(pathOfNewFile)); 
-		scene = new Scene(root); 
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow(); 
-		stage.setScene(scene);
-		stage.setMaximized(true);
-		stage.show();
+		addLink(e, pathOfNewFile);
 	}
 	public void goToGraduatePage(ActionEvent e) throws IOException{
 		String pathOfNewFile = "/pages/graduate-program.fxml";
-		root = FXMLLoader.load(getClass().getResource(pathOfNewFile));
-		scene = new Scene(root);
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-		stage.setScene(scene);
-		stage.setMaximized(true);
-		stage.show();
+		addLink(e, pathOfNewFile);
 	}
 	public void goToAcademicsPage(ActionEvent e) throws IOException{
 		String pathOfNewFile = "/pages/academics.fxml";
-		root = FXMLLoader.load(getClass().getResource(pathOfNewFile));
-		scene = new Scene(root);
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-		stage.setScene(scene);
-		stage.setMaximized(true);
-		stage.show();
+		addLink(e, pathOfNewFile);
 	}
 	public void goToEngagementPage(ActionEvent e) throws IOException{
 		String pathOfNewFile = "/pages/engagement.fxml";
-		root = FXMLLoader.load(getClass().getResource(pathOfNewFile));
-		scene = new Scene(root);
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-		stage.setScene(scene);
-		stage.setMaximized(true);
-		stage.show();
+		addLink(e, pathOfNewFile);
 	}
 	public void goToResourcesPage(ActionEvent e) throws IOException{
 		String pathOfNewFile = "/pages/resources.fxml";
-		root = FXMLLoader.load(getClass().getResource(pathOfNewFile));
-		scene = new Scene(root);
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-		stage.setScene(scene);
-		stage.setMaximized(true);
-		stage.show();
+		addLink(e, pathOfNewFile);
 	}
 	public void goToInternshipsPage(ActionEvent e) throws IOException{
 		String pathOfNewFile = "/pages/internships.fxml";
-		root = FXMLLoader.load(getClass().getResource(pathOfNewFile));
-		scene = new Scene(root);
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-		stage.setScene(scene);
-		stage.setMaximized(true);
-		stage.show();
+		addLink(e, pathOfNewFile);
 	}
 	public void goToClubsPage(ActionEvent e) throws IOException{
 		String pathOfNewFile = "/pages/clubs.fxml";
-		root = FXMLLoader.load(getClass().getResource(pathOfNewFile));
-		scene = new Scene(root);
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-		stage.setScene(scene);
-		stage.setMaximized(true);
-		stage.show();
+		addLink(e, pathOfNewFile);
 	}
 
 
@@ -106,11 +83,29 @@ public class SceneController {
 		scene = new Scene(root);
 		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
 		stage.setScene(scene);
-		stage.setMaximized(true);
+		//stage.setFullScreen(true);
 		stage.show();
 	}
 
-	public void test(ActionEvent e) throws IOException{
-		System.out.println("I work");	
+	public void test(ActionEvent e) throws IOException {
+		System.out.println("I work");
 	}
+	public void WriteStatsToFile(StopWatch watch){
+		try {
+			FileWriter myWriter = new FileWriter("TimeStats.txt");
+			myWriter.write("Total Time not on Idle: " + watch.getTime());
+			myWriter.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	private void translateGrid(int distance, TranslateTransition translate, GridPane gp) {
+		translate.setNode(gp);
+		translate.setDuration(Duration.millis(1000));
+		translate.setCycleCount(1);
+		translate.setByX(distance);
+		translate.play();
+	}
+
 }
